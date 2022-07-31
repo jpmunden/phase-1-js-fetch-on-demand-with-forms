cce915dbd2627cf3d0aee98e15ceb53b8356c585
Jacob <jacobmunden@gmail.com>
2022-07-30 18:27:11 -0600

    commit tree 85ea588eaedc4bc0c35833d1b88fe81e8a37e374

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
+/*
+
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/users/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+*/
+
+
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(name, email)
+    })
+    .then(res => res.json())
+    .then(console.log(name, email))   
+};
